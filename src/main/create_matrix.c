a77a200c7ee42e1fd3b2ff0fb6706fdb180d9e50
moon <undruee@gmail.com>
2024-11-21 23:50:48 +0300

    matrix_create func

---
+++ src/main/create_matrix.c
@@ -0,0 +1,33 @@
+#include "matrix.h"
+
+int create_matrix(int rows, int columns, matrix_t *result) {
+  int stat = ok;
+  if ((rows > INT_MAX || rows < INT_MIN) ||
+      (columns > INT_MAX || columns < INT_MIN))
+    stat = ncm_error;
+  if (result) stat = ncm_error;
+  result = (matrix_t *)malloc(sizeof(matrix_t *));
+  if (!result) stat = mem_error;
+  if (stat = ok) {
+    int **mtr = (int **)malloc(sizeof(int *) * rows);
+    if (mtr) {
+      for (int i = 0; i < rows && stat != mem_error; i++) {
+        mtr[i] = (int *)malloc(sizeof(int) * columns);
+        if (!mtr[i]) stat = mem_error;
+      }
+      if (stat != ok) {
+        for (int i = 0; i < rows; i++) {
+          if (mtr[i]) free(mtr[i]);
+        }
+        free(mtr);
+      }
+      if (stat == ok) {
+        result->matrix = mtr;
+        result->rows = rows;
+        result->columns = columns;
+      }
+    } else
+      stat = mem_error;
+  }
+  return stat;
+}
---
+++ src/main/matrix.h
@@ -0,0 +1,13 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct matrix_struct {
+  double** matrix;
+  int rows;
+  int columns;
+} matrix_t;
+enum { ok = 0, ncm_error = 1, calc_error = 2, mem_error = 3 };
+int create_matrix(int, int, matrix_t*);
+#endif
