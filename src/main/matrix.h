cd11524d542aeb05dace9fb4cb0c082b1f14de65
moon <undruee@gmail.com>
2024-11-22 20:52:38 +0300

    compare matrix func (eq_matrix)

--- src/main/matrix.h
+++ src/main/matrix.h
@@ -3,12 +3,15 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define EPSILON 0.0000001
 typedef struct matrix_struct {
   double** matrix;
   int rows;
   int columns;
 } matrix_t;
 enum { ok = 0, ncm_error = 1, calc_error = 2, mem_error = 3 };
+enum {success=1, failure=0};
 int create_matrix(int, int, matrix_t**);
 void remove_matrix(matrix_t**);
 #endif
