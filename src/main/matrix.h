331fbb4555346c248082f5f1667bf9aeec2ecc0a
moon <undruee@gmail.com>
2024-11-22 21:39:30 +0300

    sum func

--- src/main/matrix.h
+++ src/main/matrix.h
@@ -13,5 +13,8 @@ typedef struct matrix_struct {
 enum { ok = 0, ncm_error = 1, calc_error = 2, mem_error = 3 };
 enum { success = 1, failure = 0 };
 int create_matrix(int, int, matrix_t**);
+int eq_matrix(matrix_t*, matrix_t*);
+int sum_matrix(matrix_t*, matrix_t*, matrix_t**);
+int sub_matrix(matrix_t*, matrix_t*, matrix_t**);
 void remove_matrix(matrix_t**);
 #endif
---
+++ src/main/sum_matrix.c
@@ -0,0 +1,19 @@
+#include "matrix.h"
+
+int sum_matrix(matrix_t *mtr_one, matrix_t *mtr_two, matrix_t **result) {
+  int stat = ok;
+  if ((*result)) stat = ncm_error;
+  if ((!mtr_one->matrix || mtr_one->columns <= 0 || mtr_one->rows <= 0) ||
+      (!mtr_two->matrix || mtr_two->columns <= 0 || mtr_two->rows <= 0))
+    stat = ncm_error;
+  if (mtr_one->rows != mtr_two->rows || mtr_one->columns != mtr_two->columns)
+    stat = ncm_error;
+  if (stat == ok) {
+    stat = create_matrix(mtr_one->rows, mtr_one->columns, result);
+    matrix_t *res = (*result);
+    for (int i = 0; i < mtr_one->rows && stat == ok; i++)
+      for (int j = 0; j < mtr_one->columns && stat == ok; j++)
+        res->matrix[i][j] = mtr_one->matrix[i][j] + mtr_two->matrix[i][j];
+  }
+  return stat;
+}
