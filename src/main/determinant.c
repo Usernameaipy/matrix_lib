c941be1fe91c857e02243edf7b01f0c51baf3a5b
moon <undruee@gmail.com>
2024-11-25 21:57:55 +0300

    determinant func

---
+++ src/main/determinant.c
@@ -0,0 +1,43 @@
+#include "matrix.h"
+
+int determinant(matrix_t *mtr, double *result) {
+  int stat = ok;
+  if (!mtr || !mtr->matrix) stat = ncm_error;
+  if (mtr->columns != mtr->rows) stat = ncm_error;
+  if (stat == ok) {
+    *result = recurse_det(mtr);
+  }
+  return stat;
+}
+
+double recurse_det(matrix_t *mtr) {
+  double det = 0, stat = ok;
+  if (mtr->columns == 1) {
+    stat = base;
+    det = mtr->matrix[0][0];
+  }
+  if (mtr->columns == 2) {
+    stat = base;
+    det = mtr->matrix[0][0] * mtr->matrix[1][1] -
+          mtr->matrix[0][1] * mtr->matrix[1][0];
+  }
+  if (stat == ok) {
+    for (int i = 0; i < mtr->columns; i++) {
+      matrix_t *new_mtr = NULL;
+      stat = create_matrix(mtr->rows - 1, mtr->columns - 1, &new_mtr);
+      for (int j = 1; j < mtr->rows && stat == ok; j++) {
+        int k_minor = 0;
+        for (int k = 0; k < mtr->columns; k++) {
+          if (k == i) continue;
+          new_mtr->matrix[j - 1][k_minor] = mtr->matrix[j][k];
+          k_minor++;
+        }
+      }
+      if (stat == ok) {
+        det += (i % 2 == 0 ? 1 : -1) * mtr->matrix[0][i] * recurse_det(new_mtr);
+        remove_matrix(&new_mtr);
+      }
+    }
+  }
+  return det;
+}
--- src/main/matrix.h
+++ src/main/matrix.h
@@ -10,7 +10,7 @@ typedef struct matrix_struct {
   int rows;
   int columns;
 } matrix_t;
-enum { ok = 0, ncm_error = 1, calc_error = 2, mem_error = 3 };
+enum { ok = 0, ncm_error = 1, calc_error = 2, mem_error = 3, base = 4 };
 enum { success = 1, failure = 0 };
 int create_matrix(int, int, matrix_t**);
 int eq_matrix(matrix_t*, matrix_t*);
@@ -19,5 +19,7 @@ int sub_matrix(matrix_t*, matrix_t*, matrix_t**);
 int mult_number(matrix_t*, double, matrix_t**);
 int mult_matrix(matrix_t*, matrix_t*, matrix_t**);
 int transpose(matrix_t*, matrix_t**);
+int determinant(matrix_t*, double*);
+double recurse_det(matrix_t*);
 void remove_matrix(matrix_t**);
 #endif
